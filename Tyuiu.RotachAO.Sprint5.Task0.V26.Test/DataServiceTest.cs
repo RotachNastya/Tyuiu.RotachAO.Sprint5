b7d925266fe6c92fe6f5a8da4d6843dd62143038
RotachNastya <education.for.nastya@gmail.com>
2024-12-03 12:41:07 +0500

    Task0

--- Tyuiu.RotachAO.Sprint5.Task0.V26.Test/DataServiceTest.cs
+++ Tyuiu.RotachAO.Sprint5.Task0.V26.Test/DataServiceTest.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.RotachAO.Sprint5.Task0.V26.Test
     {
         [TestMethod]
         public void ValidExpression()
-        {
+        { 
             string path = @"C:\Users\user\AppData\Local\Temp\OutPutFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
