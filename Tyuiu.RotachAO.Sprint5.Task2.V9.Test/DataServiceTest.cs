ad901e4a27fa036932ba6687fe1cd95feefcd2e6
RotachNastya <education.for.nastya@gmail.com>
2024-12-12 17:23:38 +0500

    Task2

---
+++ Tyuiu.RotachAO.Sprint5.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.RotachAO.Sprint5.Task2.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.RotachAO.Sprint5.Task2.V9/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.RotachAO.Sprint5.Task2.V9.Lib;
+namespace Tyuiu.RotachAO.Sprint5.Task2.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Ротач. А. О. | ИИПб-24-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #5                                                            *");
+            Console.WriteLine("* Тема:                                                                *");
+            Console.WriteLine("* Задание #1                                                           *");
+            Console.WriteLine("* Вариант #28                                                          *");
+            Console.WriteLine("* Выполнил: Ротач Анастасия Олеговна  | ИИПб-24-1                      *");
+            Console.WriteLine("************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine($"startValue = {startValue}");
+            Console.WriteLine($"stopValue = {stopValue}");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            
+            Console.WriteLine("Файл: ");
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
