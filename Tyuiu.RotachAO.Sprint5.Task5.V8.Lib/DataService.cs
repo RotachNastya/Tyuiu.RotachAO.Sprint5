1556106c5871be458eeac66f0c8b17930d3ffe04
RotachNastya <education.for.nastya@gmail.com>
2024-12-14 16:49:01 +0500

    Task5

--- Tyuiu.RotachAO.Sprint5.Task5.V8.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint5.Task5.V8.Lib/DataService.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using tyuiu.cources.programming.interfaces.Sprint5; 
  
 namespace Tyuiu.RotachAO.Sprint5.Task5.V8.Lib;
@@ -6,22 +7,22 @@ public class DataService : ISprint5Task5V8
 {
         public double LoadFromDataFile(string path)
         {
-            string fileContent = File.ReadAllText(path);
-            var values = fileContent
-                .Split(new[] { ' ', '\n', '\r', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(val =>
+        string fileContent = File.ReadAllText(path);
+        var values = fileContent
+            .Split(new[] { ' ', '\n', '\r', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(val =>
+            {
+                if (double.TryParse(val, NumberStyles.Any, CultureInfo.InvariantCulture, out double doubleValue))
                 {
-                    if (double.TryParse(val, out double doubleValue))
-                    {
-                        return Math.Round(doubleValue, 3);
-                    }
-                    return (double?)null;
-                })
-                .Where(val => val.HasValue)
-                .Select(val => val.Value)
-                .ToArray();
-            var minInteger = values.Where(val => val == Math.Truncate(val)).Min();
-            return minInteger;
-        }
+                    return Math.Round(doubleValue, 3);
+                }
+                return (double?)null;
+            })
+            .Where(val => val.HasValue)
+            .Select(val => val.Value)
+            .ToArray();
+        var minInteger = values.Where(val => val == Math.Truncate(val)).Min();
+        return minInteger;
+    }
     }
     
